07825ccf4a7dfd331f4ef68d389bc8bdbd24ed86
Mishel Agüero <m.aguero@outlook.com>
2020-03-16 22:07:53 -0600

    Implementing messaging client

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-
+    <SendMessage/>
+    <Messages/>
   </div>
 </template>
 
 <script>
-
+import SendMessage from './components/SendMessage.vue'
+import Messages from './components/Messages.vue'
 
 export default {
   name: 'App',
   components: {
-
+    SendMessage,
+    Messages
   }
 }
 </script>
---
+++ src/components/SendMessage.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="SendMessage">
+        <form @submit="sendNewMessage">
+            <p>Username:
+                <input type="text" v-model="user">
+            </p>
+            <p>Message:
+                <textarea v-model="message"></textarea>
+            </p>
+            <button class="btn btn-success">Send</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    
+    export default {
+        name: 'SendMessage',
+        data() {
+            return {
+                user: null,
+                message: null
+            }
+        },
+        mounted() {
+            console.log('SendMessage mounted')
+        },
+        methods: {
+            sendNewMessage(e) {
+                e.preventDefault();
+                axios
+                    .post('http://localhost:8080/messages', {
+                        user: this.user,
+                        message: this.message
+                    })
+                    .then(response => {
+                        console.log(response.data)
+                    })
+            }
+        }
+    }
+</script>
